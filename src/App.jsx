19db4ec097a010bbc261f0c3dc81b4f78ce843b0
mlang1988 <165694522+mlang1988@users.noreply.github.com>
2026-04-02 11:59:48 +0200

    Added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 export default function App() {
-  return <div>Dictionary Project</div>;
+  return (
+    <div className="App">
+      <h1>Dictionary Project</h1>
+    </div>
+  );
 }
