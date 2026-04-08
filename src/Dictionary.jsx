60d9e78e7e14b01e060cb512941832cdb0ca8a78
mlang1988 <165694522+mlang1988@users.noreply.github.com>
2026-04-08 11:19:54 +0200

    Added results

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
@@ -28,6 +31,7 @@ export default function Dictionary() {
         <input type="search" onChange={handleChange} />
         <input type="submit" value="Search" />
       </form>
+      <Results results={results} />
     </div>
   );
 }
